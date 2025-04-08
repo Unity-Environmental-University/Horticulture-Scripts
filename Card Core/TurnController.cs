288d123839df51852a10e98ab8c9a7e8ed93bf3f
Donovan Montoya <donovan.montoya1@gmail.com>
2025-04-08 16:19:29 -0400

    Update turns instead of rounds.
    EndTurn method now handles PlantControllers in PlantLocation, drawing a new hand and curing afflicted controllers. Additionally, it randomly spreads afflictions to other plant controllers with a 50% chance.
    
    Signed-off-by: Donovan Montoya <donovan.montoya1@gmail.com>

--- Card Core/TurnController.cs
+++ Card Core/TurnController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Linq;
 using _project.Scripts.Core;
 using UnityEngine;
+using Random = System.Random;
 
 namespace _project.Scripts.Card_Core
 {
@@ -54,7 +55,45 @@ namespace _project.Scripts.Card_Core
             }
         }
 
-        public void EndRound()
+        public void EndTurn()
+        {
+            // Get PlantControllers in PlantLocation.
+            var plantControllers = deckManager.plantLocations
+                .SelectMany(location => location.GetComponentsInChildren<PlantController>(false))
+                .ToArray();
+            if (debugging) Debug.Log($"Found {plantControllers.Length} PlantControllers in PlantLocation.");
+
+            // Drawn new Hand
+            deckManager.DrawActionHand();
+
+            // Cure queued afflictions
+            foreach (var controller in plantControllers)
+            {
+                controller.plantCardFunctions.ApplyQueuedTreatments();
+                controller.FlagShadersUpdate();
+            }
+
+            // Randomly check if a plant spreads an affliction from CurrentAfflictions to another plant controller
+            var random = new Random();
+            foreach (var controller in plantControllers)
+            {
+                if (!controller.CurrentAfflictions.Any() || !(random.NextDouble() < 0.5)) continue; // 50% chance
+                var targetController = plantControllers
+                    .Where(c => c != controller)
+                    .OrderBy(_ => random.Next())
+                    .FirstOrDefault();
+
+                if (!targetController) continue;
+                var affliction = controller.CurrentAfflictions.First();
+                if (targetController == null) continue;
+                targetController.AddAffliction(affliction);
+                targetController.FlagShadersUpdate();
+                if (debugging)
+                    Debug.Log($"Affliction {affliction} spread from {controller.name} to {targetController.name}.");
+            }
+        }
+
+    public void EndRound()
         {
             deckManager.ClearActionHand();
             var score = scoreManager.CalculateScore();
