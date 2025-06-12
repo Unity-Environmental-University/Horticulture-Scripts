46d5e93c658eed2fd38d56863cc5bfcda7496c9b
Donovan Montoya <dmontoya@unity.edu>
2025-06-12 16:07:45 -0400

    Implement Shop Inventory Generation with ShopObject & Updated ShopManager
    Introduces a new `ShopObject` and `ShopManager` scripts to dynamically generate shop inventory.
    
    Signed-off-by: Donovan Montoya <dmontoya@unity.edu>

--- Card Core/ShopManager.cs
+++ Card Core/ShopManager.cs
@@ -1,34 +1,61 @@
 using System;
+using System.Collections.Generic;
 using _project.Scripts.Classes;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace _project.Scripts.Card_Core
 {
     public class ShopManager : MonoBehaviour
     {
+        [SerializeField] private GameObject shopItemsParent;
+        [SerializeField] private GameObject shopItemPrefab;
+        [SerializeField] private int numberOfCards = 4;
+
         public GameObject shopPanel;
         public bool isShopOpen;
-        private void Start(){}
-        private void Update(){}
+
+        private List<ICard> availableCards;
+
+        private void Start() { GenerateShopInventory(); }
 
         public void OpenShop()
         {
-            throw new NotImplementedException();
+            shopPanel.SetActive(true);
+            isShopOpen = true;
         }
 
         public void CloseShop()
         {
-            throw new NotImplementedException();
+            shopPanel.SetActive(false);
+            isShopOpen = false;
         }
 
-        private void DisplayShopOptions()
+        private void GenerateShopInventory()
         {
-            throw new NotImplementedException();
+            ClearShop();
+
+            availableCards = new List<ICard>
+            {
+                new NeemOilBasic(),
+                new SoapyWaterBasic(),
+                new FungicideBasic(),
+                new InsecticideBasic(),
+                new Panacea()
+            };
+
+            for (var i = 0; i < numberOfCards; i++)
+            {
+                var randCard = availableCards[Random.Range(0, availableCards.Count)].Clone();
+                var cardObj = Instantiate(shopItemPrefab, shopItemsParent.transform);
+                var cardView = cardObj.GetComponent<CardView>();
+                cardView.Setup(randCard);
+
+                var shopItem = cardObj.AddComponent<ShopObject>();
+                shopItem.Setup(randCard, this);
+            }
         }
 
-        private void PurchaseOption(ICard card)
-        {
-            throw new NotImplementedException();
-        }
+        private static void ClearShop() { throw new NotImplementedException(); }
     }
 }
