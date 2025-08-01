e16ffdfc031d312494283af2921920aa48827ae4
Donovan Montoya <dmontoya@unity.edu>
2025-08-01 16:12:19 -0400

    Extended `ISticker` interface and added `CopySticker` class implementation in `StickerClasses`.

--- Classes/StickerClasses.cs
+++ Classes/StickerClasses.cs
@@ -3,23 +3,39 @@ using UnityEngine;
 
 namespace _project.Scripts.Classes
 {
-    public class StickerClasses
-    {
-        public interface ISticker
-        {
-            string Name { get; }
-            string Description => null;
-            int? Value
-            {
-                get => null;
-                set => throw new NotImplementedException();
-            }
-            
-            GameObject Prefab => null;
-            Material Material => null;
-            ISticker Clone();
+    #region Declairation
 
-            public void Selected() { }
+    public interface ISticker
+    {
+        string Name { get; }
+        string Description => null;
+
+        int? Value
+        {
+            get => null;
+            set => throw new NotImplementedException();
         }
+
+        GameObject Prefab => null;
+        Material Material => null;
+        ISticker Clone();
+
+        public void Selected() { throw new NotImplementedException(); }
+        public void Apply() { throw new NotImplementedException(); }
     }
+
+    #endregion
+
+    #region CardStickers
+
+    public class CopySticker : ISticker
+    {
+        public string Name => "";
+        public string Description => "";
+        public int Value => 0;
+        
+        public ISticker Clone() { return new CopySticker(); }
+    }
+
+    #endregion
 }
