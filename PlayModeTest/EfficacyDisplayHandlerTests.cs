0c7de5e4371f9dbc5a7e32f9409fba899c0de17f
Donovan Montoya <dmontoya@unity.edu>
2026-01-15 13:22:06 -0500

    Remove unused test for LadyBugs efficacy display in `EfficacyDisplayHandlerTests`

--- PlayModeTest/EfficacyDisplayHandlerTests.cs
+++ PlayModeTest/EfficacyDisplayHandlerTests.cs
@@ -43,36 +43,5 @@ namespace _project.Scripts.PlayModeTest
 
             yield return null;
         }
-
-        [UnityTest]
-        public IEnumerator UpdateInfo_DoesNotShowEfficacyForLadyBugs()
-        {
-            var plantGo = new GameObject("Plant");
-            var plant = plantGo.AddComponent<PlantController>();
-            plant.PlantCard = new ColeusCard();
-
-            var affliction = new PlantAfflictions.SpiderMitesAffliction();
-            plant.AddAffliction(affliction);
-
-            var displayGo = new GameObject("EfficacyDisplay");
-            var efficacyText = displayGo.AddComponent<TextMeshPro>();
-            var displayHandler = displayGo.AddComponent<EfficacyDisplayHandler>();
-
-            typeof(EfficacyDisplayHandler)
-                .GetField("efficacyText", BindingFlags.Instance | BindingFlags.NonPublic)
-                ?.SetValue(displayHandler, efficacyText);
-
-            displayHandler.SetPlant(plant);
-            displayHandler.SetTreatment(new PlantAfflictions.LadyBugs());
-
-            displayHandler.UpdateInfo();
-
-            Assert.AreEqual(string.Empty, efficacyText.text);
-
-            Object.Destroy(displayGo);
-            Object.Destroy(plantGo);
-
-            yield return null;
-        }
     }
 }
